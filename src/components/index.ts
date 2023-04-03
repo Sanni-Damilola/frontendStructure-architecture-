9f51e390b59b8868a72fe486e9013d242dd805e0
Sanni-Damilola <sannifortune11@gmail.com>
2023-04-03 14:25:56 +0100

    update

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+/** @format */
+
+export * from "./blocks";
+// export * from "./blocks"
+export * from "./layout";
---
+++ src/components/layout/HomeLayout/HomeLayout.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import { Footer, Header } from "../../blocks/homeBlocks";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <Header />
+      
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeLayout;
