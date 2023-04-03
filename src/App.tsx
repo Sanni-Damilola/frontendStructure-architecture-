55f4a7e7205ebd040165feffab12693dda196d2f
Sanni-Damilola <sannifortune11@gmail.com>
2023-04-03 14:08:25 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import "./App.css"
+/** @format */
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -14,8 +16,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
