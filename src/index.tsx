14c3bbecbca1db1e242994b47a651e597778341f
Sanni-Damilola <sannifortune11@gmail.com>
2023-04-04 15:25:30 +0100

    7th Round Started

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,13 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import { Loading } from "./utils/hoc";
-import { RouterProvider } from "react-router-dom";
-import { element } from "./routes/AllRoutes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={element}></RouterProvider>
-    </Suspense>
+    <App />
   </React.StrictMode>,
 );
 
